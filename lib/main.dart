fd18a02a3903e2276d0bf3daaa787c14f01fa524
quirinof <matheusquinoff@gmail.com>
2023-08-30 16:08:12 -0300

    update recipe1

--- lib/main.dart
+++ lib/main.dart
@@ -4,18 +4,38 @@ void main() {
   MaterialApp app = MaterialApp(
       theme: ThemeData(primarySwatch: Colors.cyan),
       home: Scaffold(
-          appBar: AppBar(
-            title: const Center(child: Text("MY APP")),
-          ),
-          body: const Center(
-              child: Column(
-            children: [
-              Text("Just starting..."),
-              Text("in middle..."),
-              Text("ending.")
-            ],
+        appBar: AppBar(
+          title: const Center(
+              child: Text(
+            "Campeonato Brasileiro",
+            style: TextStyle(fontSize: 30),
           )),
-          bottomNavigationBar: const Text("Next")));
+        ),
+        body: const Center(
+            child: Column(
+          children: [
+            Text("Classificação...",
+                style:
+                    TextStyle(color: Colors.blue, fontWeight: FontWeight.bold)),
+            Text("Partidas..."),
+            Text("Noticias...")
+          ],
+        )),
+        bottomNavigationBar: Row(children: <Widget>[
+          ElevatedButton.icon(
+              onPressed: () {},
+              icon: const Icon(Icons.newspaper),
+              label: const Text("Notícias")),
+          ElevatedButton.icon(
+              onPressed: () {},
+              icon: const Icon(Icons.menu),
+              label: const Text("Menu")),
+          ElevatedButton.icon(
+              onPressed: () {},
+              icon: const Icon(Icons.loop),
+              label: const Text("Transferências"))
+        ]),
+      ));
 
   runApp(app);
 }
