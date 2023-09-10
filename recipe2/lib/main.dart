bd8a47b25ae9acc0e5592c03f0c3718823177b6c
quirinof <matheusquinoff@gmail.com>
2023-09-10 15:06:47 -0300

    update recipe2

--- recipe2/lib/main.dart
+++ recipe2/lib/main.dart
@@ -1,46 +1,71 @@
 import 'package:flutter/material.dart';
 
-class CaixaDeIcones extends StatelessWidget {
+class TitAppBar extends StatelessWidget {
+  TitAppBar();
 
-  CaixaDeIcones();
-
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: Text("Dicas")
+    );
+  }
 }
+class ColumnsBody extends StatelessWidget {
+  ColumnsBody();
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(children: [
+      Expanded(
+        child: Text("La Fin Du Monde | Bock | 65 ibu"),
+      ),
+      Expanded(
+        child: Text("Sapporo Premiume | Sour Ale | 54 ibu"),
+      ),
+      Expanded(
+        child: Text("Duvel | Pilsner | 82 ibu")
+      )
+    ]);
+  }
+}
+
+class NavBar extends StatelessWidget {
+  NavBar();
+
+  void touchedButton(int index) {
+    print("botao $index tocado");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      onTap: touchedButton, 
+      items: const [
+        BottomNavigationBarItem(
+          label: "Cafés",
+          icon: Icon(Icons.coffee_outlined),
+        ),
+        BottomNavigationBarItem(
+          label: "Cervejas", 
+          icon: Icon(Icons.local_drink_outlined)),
+        BottomNavigationBarItem(
+          label: "Nações", 
+          icon: Icon(Icons.flag_outlined))
+      ]
+    );
+  }
+}
+
 void main() {
-
   MaterialApp app = MaterialApp(
-
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
-
-      home: Scaffold(
-
-        appBar: AppBar(title: const Text("Dicas")),
-
-        body: const Column(children: [
-
-          Expanded(
-
-            child: Text("La Fin Du Monde - Bock - 65 ibu"),
-
-          ),
-
-          Expanded(
-
-            child: Text("Sapporo Premiume - Sour Ale - 54 ibu"),
-
-          ),
-
-          Expanded(
-
-            child: Text("Duvel - Pilsner - 82 ibu"),
-
-          )
-
-        ]),
-
-        bottomNavigationBar: CaixaDeIcones()),
-
-      ));
-
+    theme: ThemeData(primarySwatch: Colors.cyan),
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("Dicas")
+      ),
+      body: ColumnsBody(),
+      bottomNavigationBar: NavBar()
+    )
+  );
   runApp(app);
-
 }
