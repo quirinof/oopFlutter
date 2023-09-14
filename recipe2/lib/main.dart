194f079e8482621564ea84d1ddec2fad5ed46094
quirinof <matheusquinoff@gmail.com>
2023-09-14 12:42:42 -0300

    finishing recipe2

--- recipe2/lib/main.dart
+++ recipe2/lib/main.dart
@@ -1,15 +1,39 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  MyApp app = MyApp();
+  runApp(app);
+}
+
+class MyApp extends StatelessWidget {
+  MyApp();
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.cyan),
+      home: Scaffold(
+        appBar: AppBar(title: const Text("Dicas")),
+        body: ColumnsBody(),
+        bottomNavigationBar: NavBar(icons: const[
+          Icon(Icons.coffee_outlined),
+          Icon(Icons.local_drink_outlined),
+          Icon(Icons.flag_outlined)
+        ])
+      )
+    );
+  }
+}
+
+/*
 class TitAppBar extends StatelessWidget {
   TitAppBar();
 
   @override
   Widget build(BuildContext context) {
-    return AppBar(
-      title: Text("Dicas")
-    );
+    return AppBar(title: Text("Dicas"));
   }
 }
+*/
 class ColumnsBody extends StatelessWidget {
   ColumnsBody();
 
@@ -17,20 +41,20 @@ class ColumnsBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Column(children: [
       Expanded(
-        child: Text("La Fin Du Monde | Bock | 65 ibu"),
+        child: Center(child: Text("La Fin Du Monde | Bock | 65 ibu")),
       ),
       Expanded(
-        child: Text("Sapporo Premiume | Sour Ale | 54 ibu"),
+        child: Center(child: Text("Sapporo Premiume | Sour Ale | 54 ibu")),
       ),
       Expanded(
-        child: Text("Duvel | Pilsner | 82 ibu")
-      )
+        child: Center(child: Text("Duvel | Pilsner | 82 ibu")))
     ]);
   }
 }
 
 class NavBar extends StatelessWidget {
-  NavBar();
+  List<Icon> icons;
+  NavBar({this.icons = const []});
 
   void touchedButton(int index) {
     print("botao $index tocado");
@@ -39,33 +63,14 @@ class NavBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
-      onTap: touchedButton, 
-      items: const [
-        BottomNavigationBarItem(
-          label: "Cafés",
-          icon: Icon(Icons.coffee_outlined),
-        ),
-        BottomNavigationBarItem(
-          label: "Cervejas", 
-          icon: Icon(Icons.local_drink_outlined)),
-        BottomNavigationBarItem(
-          label: "Nações", 
-          icon: Icon(Icons.flag_outlined))
-      ]
+      onTap: touchedButton,
+      items: List.generate(
+        icons.length,
+        (index) => BottomNavigationBarItem(
+          icon: icons[index],
+          label: 'name $index' // Pode personalizar os rótulos conforme necessário
+        )
+      )
     );
   }
-}
-
-void main() {
-  MaterialApp app = MaterialApp(
-    theme: ThemeData(primarySwatch: Colors.cyan),
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text("Dicas")
-      ),
-      body: ColumnsBody(),
-      bottomNavigationBar: NavBar()
-    )
-  );
-  runApp(app);
 }
