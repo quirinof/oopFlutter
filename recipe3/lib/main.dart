ae356f22a3fbc38b1df7cc8c7514cb341ef862d4
quirinof <matheusquinoff@gmail.com>
2023-09-15 12:55:36 -0300

    update recipe3

--- recipe3/lib/main.dart
+++ recipe3/lib/main.dart
@@ -15,49 +15,77 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           title: const Text("Dicas"),
+          actions: <Widget>[
+            PopupMenuButton<String>(
+              itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
+                const PopupMenuItem<String>(
+                  child: ListTile(
+                    leading: Icon(Icons.color_lens_outlined),
+                    title: Text('Azul')
+                  )
+                ),
+                const PopupMenuItem<String>(
+                  child: ListTile(
+                    leading: Icon(Icons.color_lens),
+                    title: Text('Verde')
+                  )
+                ),
+                const PopupMenuItem<String>(
+                  child: ListTile(
+                    leading: Icon(Icons.color_lens),
+                    title: Text('Vermelho')
+                  )
+                )
+              ]
+            )
+          ]
         ),
         body: DataBodyWidget(objects: const [
           "La Fin Du Monde | Bock | 65 ibu",
           "Sapporo Premiume | Sour Ale | 54 ibu",
           "Duvel | Pilsner | 82 ibu"
         ]),
-        bottomNavigationBar: NewNavBar(),
+        bottomNavigationBar: NewNavBar(
+          icons: const[
+            Icon(Icons.coffee_outlined),
+            Icon(Icons.local_drink_outlined),
+            Icon(Icons.flag_outlined)
+          ]
+        ) 
       )
     );
   }
 }
 
 class NewNavBar extends StatelessWidget {
-  NewNavBar();
+  List<Icon> icons;
+  NewNavBar({this.icons = const[]});
+
   void botaoFoiTocado(int index) {
     print("Tocaram no botão $index");
   }
-
+  /*
+  BottomNavigationBarItem processarUmIcone(Icon icone) {
+    return BottomNavigationBarItem(icon: icone);
+  }
+  */
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
       onTap: botaoFoiTocado, 
-      items: const [
-        BottomNavigationBarItem(
-          label: "Cafés",
-          icon: Icon(Icons.coffee_outlined),
-        ),
-        BottomNavigationBarItem(
-          label: "Cervejas", 
-          icon: Icon(Icons.local_drink_outlined)
-        ),
-        BottomNavigationBarItem(
-          label: "Nações", 
-          icon: Icon(Icons.flag_outlined)
+      items: icons.map(
+        (icn) => BottomNavigationBarItem(
+          label: "name",
+          icon: icn
         )
-      ]
+      ).toList()
     );
   }
 }
 
 class DataBodyWidget extends StatelessWidget {
   List<String> objects;
-  DataBodyWidget({this.objects = const []});
+  DataBodyWidget({this.objects = const[]});
   /*
   Expanded processarUmElemento(String obj){
     return Expanded(                
@@ -67,10 +95,10 @@ class DataBodyWidget extends StatelessWidget {
   */
   @override
   Widget build(BuildContext context) {
-    return Column(children: objects.map(
-      (obj) => Expanded(
-        child: Center(child: Text(obj))
-      )
-    ).toList());
+    return Column(
+      children: objects.map(
+        (obj) => Expanded(child: Center(child: Text(obj)))
+      ).toList()
+    );
   }
 }
