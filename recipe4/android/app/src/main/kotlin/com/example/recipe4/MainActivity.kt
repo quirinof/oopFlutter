c0f9fc18da77313f4fbc702abf07a9f9ba0e4a4e
quirinof <matheusquinoff@gmail.com>
2023-09-19 11:34:33 -0300

    starting recipe4

---
+++ recipe4/android/app/src/main/kotlin/com/example/recipe4/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.recipe4
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ recipe4/lib/main.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+var dataObjects = [
+  {
+    "name": "La Fin Du Monde",
+    "style": "Bock",
+    "ibu": "65"
+  },
+  {
+    "name": "Sapporo Premiume",
+    "style": "Sour Al",
+    "ibu": "54"
+  },
+  {
+    "name": "Duvel",
+    "style": "Pilsner",
+    "ibu": "82"
+  }
+];
+
+void main() {
+  MyApp app = MyApp();
+  runApp(app);
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData(primarySwatch: Colors.deepPurple),
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+            appBar: AppBar(
+              title: const Text("Dicas"),
+            ),
+            body: DataBodyWidget(objects: dataObjects),
+            bottomNavigationBar: NewNavBar()));
+  }
+}
+
+class NewNavBar extends StatelessWidget {
+  NewNavBar();
+  void botaoFoiTocado(int index) {
+    print("Tocaram no botão $index");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(onTap: botaoFoiTocado, items: const [
+      BottomNavigationBarItem(
+        label: "Cafés",
+        icon: Icon(Icons.coffee_outlined),
+      ),
+      BottomNavigationBarItem(
+          label: "Cervejas", icon: Icon(Icons.local_drink_outlined)),
+      BottomNavigationBarItem(label: "Nações", icon: Icon(Icons.flag_outlined))
+    ]);
+  }
+}
+
+class DataBodyWidget extends StatelessWidget {
+  List objects;
+  DataBodyWidget({this.objects = const []});
+
+  @override
+  Widget build(BuildContext context) {
+    var columnNames = ["Nome", "Estilo", "IBU"],
+        propertyNames = ["name", "style", "ibu"];
+    return DataTable(
+      columns: columnNames.map(
+        (name) => DataColumn(
+          label: Expanded(
+            child: Text(name, style: const TextStyle(fontStyle: FontStyle.italic))
+          )
+        )
+      ).toList(),
+      rows: objects.map(
+        (obj) => DataRow(
+          cells: propertyNames.map(
+            (propName) => DataCell(Text(obj[propName]))
+          ).toList()
+        )
+      ).toList()
+    );
+  }
+}
