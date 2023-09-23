a07a13f927454257403c426cfc82779924d2e014
quirinof <matheusquinoff@gmail.com>
2023-09-23 15:29:08 -0300

    update recipe5

--- recipe5/lib/main.dart
+++ recipe5/lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
 var dataObjects = [];
-
-
 
 void main() {
   MyApp app = MyApp();
@@ -12,62 +11,69 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    print("no build da classe MyApp");
     return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
-      debugShowCheckedModeBanner:false,
-      home: Scaffold(
-        appBar: AppBar( 
-          title: const Text("Dicas"),
+        theme: ThemeData(primarySwatch: Colors.deepPurple),
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text("Dicas"),
           ),
-        body: DataTableWidget(jsonObjects:dataObjects),
-        bottomNavigationBar: NewNavBar(),
-      ));
+          body: DataTableWidget(jsonObjects: dataObjects),
+          bottomNavigationBar: NewNavBar(),
+        ));
   }
 }
 
-class NewNavBar extends StatelessWidget {
+class NewNavBar extends HookWidget {
   NewNavBar();
-  void buttonTapped(int index) {
-    print("Tocaram no botão $index");
-  }
+
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(onTap: buttonTapped, items: const [
-      BottomNavigationBarItem(
-        label: "Cafés",
-        icon: Icon(Icons.coffee_outlined),
-      ),
-      BottomNavigationBarItem(
-          label: "Cervejas", icon: Icon(Icons.local_drink_outlined)),
-      BottomNavigationBarItem(label: "Nações", icon: Icon(Icons.flag_outlined))
-    ]);
+    print("no build da classe NewNavBar");
+    var state = useState(2);
+    return BottomNavigationBar(
+        onTap: (index) {
+          state.value = index;
+        },
+        currentIndex: state.value,
+        items: const [
+          BottomNavigationBarItem(
+            label: "Cafés",
+            icon: Icon(Icons.coffee_outlined),
+          ),
+          BottomNavigationBarItem(
+              label: "Cervejas", icon: Icon(Icons.local_drink_outlined)),
+          BottomNavigationBarItem(
+              label: "Nações", icon: Icon(Icons.flag_outlined))
+        ]);
   }
 }
 
 class DataTableWidget extends StatelessWidget {
   final List jsonObjects;
-  DataTableWidget( {this.jsonObjects = const [] });
+  DataTableWidget({this.jsonObjects = const []});
   @override
-  Widget build(BuildContext context) {    
-    var columnNames = ["Nome","Estilo","IBU"],
-        propertyNames = ["name", "style", "ibu"];    
+  Widget build(BuildContext context) {
+    print("no build da classe DataTableWidget");
+    var columnNames = ["Nome", "Estilo", "IBU"],
+        propertyNames = ["name", "style", "ibu"];
     return DataTable(
-      columns: columnNames.map( 
-                (name) => DataColumn(
-                  label: Expanded(
-                    child: Text(
-                      name, 
-                      style: const TextStyle(fontStyle: FontStyle.italic)
-                    )
-                  )
-                )
-              ).toList(),
-      rows: jsonObjects.map( 
-        (obj) => DataRow(
-            cells: propertyNames.map(
-              (propName) => DataCell(Text(obj[propName]))
-            ).toList()
-          )
-        ).toList());
+        columns: columnNames
+            .map((name) => DataColumn(
+                label: Expanded(
+                    child: Text(name,
+                        style: const TextStyle(fontStyle: FontStyle.italic)))))
+            .toList(),
+        rows: jsonObjects
+            .map((obj) => DataRow(
+                cells: propertyNames
+                    .map((propName) => DataCell(Text(obj[propName])))
+                    .toList()))
+            .toList());
   }
 }
+
+// Analisando os logs percebemos que o build do MyApp, DataBodyWidget e
+// NewNavBar são acionados de início, porém após os taps nos botões inferiores,
+// o build NewNavBar é acionado quantas vezes vc clicar.
