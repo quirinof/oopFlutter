fe4fe95fbd5f2b53ff4e83a6d312a0446e1fd88e
quirinof <matheusquinoff@gmail.com>
2023-09-23 17:25:44 -0300

    finishing recipe5

--- recipe5/lib/main.dart
+++ recipe5/lib/main.dart
@@ -13,15 +13,16 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     print("no build da classe MyApp");
     return MaterialApp(
-        theme: ThemeData(primarySwatch: Colors.deepPurple),
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          appBar: AppBar(
-            title: const Text("Dicas"),
-          ),
-          body: DataTableWidget(jsonObjects: dataObjects),
-          bottomNavigationBar: NewNavBar(),
-        ));
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Dicas"),
+        ),
+        body: DataTableWidget(jsonObjects: dataObjects),
+        bottomNavigationBar: NewNavBar2()
+      )
+    );
   }
 }
 
@@ -33,20 +34,61 @@ class NewNavBar extends HookWidget {
     print("no build da classe NewNavBar");
     var state = useState(2);
     return BottomNavigationBar(
-        onTap: (index) {
-          state.value = index;
-        },
-        currentIndex: state.value,
-        items: const [
-          BottomNavigationBarItem(
-            label: "Cafés",
-            icon: Icon(Icons.coffee_outlined),
-          ),
-          BottomNavigationBarItem(
-              label: "Cervejas", icon: Icon(Icons.local_drink_outlined)),
-          BottomNavigationBarItem(
-              label: "Nações", icon: Icon(Icons.flag_outlined))
-        ]);
+      onTap: (index) {
+        state.value = index;
+      },
+      currentIndex: state.value,
+      items: const [
+        BottomNavigationBarItem(
+          label: "Cafés",
+          icon: Icon(Icons.coffee_outlined),
+        ),
+        BottomNavigationBarItem(
+          label: "Cervejas", 
+          icon: Icon(Icons.local_drink_outlined)
+        ),
+        BottomNavigationBarItem(
+          label: "Nações", 
+          icon: Icon(Icons.flag_outlined)
+        )
+      ]
+    );
+  }
+}
+
+class NewNavBar2 extends StatefulWidget {
+  @override
+  NewNavBarState createState() => NewNavBarState();
+}
+
+class NewNavBarState extends State<NewNavBar2> {
+  int cIndex = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    print("no build da classe NewNavBar2");
+    return BottomNavigationBar(
+      onTap: (index) {
+        setState(() {
+          cIndex = index;
+        });
+      },
+      currentIndex: cIndex,
+      items: const [
+        BottomNavigationBarItem(
+          label: "Cafés",
+          icon: Icon(Icons.coffee_outlined),
+        ),
+        BottomNavigationBarItem(
+          label: "Cervejas", 
+          icon: Icon(Icons.local_drink_outlined)
+        ),
+        BottomNavigationBarItem(
+          label: "Nações", 
+          icon: Icon(Icons.flag_outlined)
+        )
+      ]
+    );
   }
 }
 
@@ -59,21 +101,22 @@ class DataTableWidget extends StatelessWidget {
     var columnNames = ["Nome", "Estilo", "IBU"],
         propertyNames = ["name", "style", "ibu"];
     return DataTable(
-        columns: columnNames
-            .map((name) => DataColumn(
-                label: Expanded(
-                    child: Text(name,
-                        style: const TextStyle(fontStyle: FontStyle.italic)))))
-            .toList(),
-        rows: jsonObjects
-            .map((obj) => DataRow(
-                cells: propertyNames
-                    .map((propName) => DataCell(Text(obj[propName])))
-                    .toList()))
-            .toList());
+      columns: columnNames
+        .map((name) => DataColumn(
+          label: Expanded(
+            child: Text(name,
+              style: const TextStyle(fontStyle: FontStyle.italic)))))
+        .toList(),
+      rows: jsonObjects
+        .map((obj) => DataRow(
+          cells: propertyNames
+            .map((propName) => DataCell(Text(obj[propName])))
+            .toList()))
+        .toList()
+    );
   }
 }
 
 // Analisando os logs percebemos que o build do MyApp, DataBodyWidget e
 // NewNavBar são acionados de início, porém após os taps nos botões inferiores,
-// o build NewNavBar é acionado quantas vezes vc clicar.
+// apenas o build NewNavBar é acionado, quantas vezes vc clicar.
