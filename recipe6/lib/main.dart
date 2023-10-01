73131ac9249c26cf988146e2920d0326d8ce5ec7
quirinof <matheusquinoff@gmail.com>
2023-09-30 22:47:18 -0300

    update recipe6

--- recipe6/lib/main.dart
+++ recipe6/lib/main.dart
@@ -2,8 +2,37 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
-final ValueNotifier<List> twitter = new ValueNotifier([]);
+class DataService {
+  final ValueNotifier<List> tableStateNotifier = ValueNotifier([]);
+
+  void carregar(index){
+    if (index == 1) carregarCervejas();
+  }
+
+  void carregarCervejas(){
+    tableStateNotifier.value = [
+      {
+      "name": "La Fin Du Monde",
+      "style": "Bock",
+      "ibu": "65"
+      },
+      {
+      "name": "Sapporo Premiume",
+      "style": "Sour Ale",
+      "ibu": "54"
+      },
+      {
+      "name": "Duvel", 
+      "style": "Pilsner", 
+      "ibu": "82"
+      }
+    ];
+  }
+}
+final dataService = DataService();
+
 //var dataObjects = [];
+
 
 void main() {
   MyApp app = MyApp();
@@ -14,44 +43,60 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        theme: ThemeData(primarySwatch: Colors.deepPurple),
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          appBar: AppBar(
-            title: const Text("Dicas"),
-          ),
-          body: DataTableWidget(jsonObjects: twitter.value),
-          bottomNavigationBar: NewNavBar(),
-        ));
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Dicas"),
+        ),
+        body: ValueListenableBuilder(
+          valueListenable: dataService.tableStateNotifier,
+          builder:(_, value, __){
+            return DataTableWidget(
+              jsonObjects: value, 
+              propertyNames: const ["name", "style", "ibu"], 
+              columnNames: const ["Nome", "Estilo", "IBU"]
+            );
+          }
+        ),
+        bottomNavigationBar: NewNavBar(
+          itemSelectedCallback: dataService.carregar
+        )
+      ));
   }
 }
 
 class NewNavBar extends HookWidget {
-  NewNavBar();
+  var itemSelectedCallback;
+
+  NewNavBar({this.itemSelectedCallback}){
+    itemSelectedCallback ??= (_){};
+  }
 
   @override
   Widget build(BuildContext context) {
     var state = useState(1);
     return BottomNavigationBar(
-        onTap: (index) {
-          state.value = index;
-          twitter.value = [
-            {"name": "La Fin Du Monde", "style": "Bock", "ibu": "65"},
-            {"name": "Sapporo Premiume", "style": "Sour Ale", "ibu": "54"},
-            {"name": "Duvel", "style": "Pilsner", "ibu": "82"}
-          ];
-        },
-        currentIndex: state.value,
-        items: const [
-          BottomNavigationBarItem(
-            label: "Cafés",
-            icon: Icon(Icons.coffee_outlined),
-          ),
-          BottomNavigationBarItem(
-              label: "Cervejas", icon: Icon(Icons.local_drink_outlined)),
-          BottomNavigationBarItem(
-              label: "Nações", icon: Icon(Icons.flag_outlined))
-        ]);
+      onTap: (index) {
+        state.value = index;
+        itemSelectedCallback(index);
+        //carregarCervejas();
+      },
+      currentIndex: state.value,
+      items: const [
+        BottomNavigationBarItem(
+          label: "Cafés",
+          icon: Icon(Icons.coffee_outlined),
+        ),
+        BottomNavigationBarItem(
+          label: "Cervejas", 
+          icon: Icon(Icons.local_drink_outlined)
+        ),
+        BottomNavigationBarItem(
+          label: "Nações", 
+          icon: Icon(Icons.flag_outlined)
+        )
+      ]);
   }
 }
 
@@ -60,25 +105,31 @@ class DataTableWidget extends StatelessWidget {
   final List<String> columnNames;
   final List<String> propertyNames;
 
-  DataTableWidget(
-      {this.jsonObjects = const [],
-      this.columnNames = const ["Nome", "Estilo", "IBU"],
-      this.propertyNames = const ["name", "style", "ibu"]});
+  DataTableWidget({
+    this.jsonObjects = const [],
+    this.columnNames = const ["Nome", "Estilo", "IBU"],
+    this.propertyNames = const ["name", "style", "ibu"]
+  });
 
   @override
   Widget build(BuildContext context) {
     return DataTable(
-        columns: columnNames
-            .map((name) => DataColumn(
-                label: Expanded(
-                    child: Text(name,
-                        style: const TextStyle(fontStyle: FontStyle.italic)))))
-            .toList(),
-        rows: jsonObjects
-            .map((obj) => DataRow(
-                cells: propertyNames
-                    .map((propName) => DataCell(Text(obj[propName])))
-                    .toList()))
-            .toList());
+      columns: columnNames.map(
+        (name) => DataColumn(
+          label: Expanded(
+            child: Text(
+              name,
+              style: const TextStyle(fontStyle: FontStyle.italic)
+            )
+          )
+        )
+      ).toList(),
+      rows: jsonObjects.map(
+        (obj) => DataRow(
+          cells: propertyNames.map((
+            propName) => DataCell(Text(obj[propName]))
+          ).toList()
+        )
+      ).toList());
   }
 }
