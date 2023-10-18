c35e908ad397be30a238bdf4b2f9d5ca55891066
quirinof <matheusquinoff@gmail.com>
2023-10-18 18:41:52 -0300

    finishing recipe7

--- recipe7/lib/main.dart
+++ recipe7/lib/main.dart
@@ -7,6 +7,7 @@ class DataService {
   final ValueNotifier<List> tableStateNotifier = ValueNotifier([]);
   List<String> columnNames = ["Nome", "Estilo", "IBU"];
   List<String> propertyNames = ["name", "style", "ibu"];
+  int sizeSelected = 10;
 
   void carregar(index) {
     /*  print('carregar #1 - antes de carregarCervejas'); */
@@ -32,10 +33,11 @@ class DataService {
 
   Future<void> carregarCafes() async {
     var coffeesUri = Uri(
-        scheme: 'https',
-        host: 'random-data-api.com',
-        path: 'api/coffee/random_coffee',
-        queryParameters: {'size': '5'});
+      scheme: 'https',
+      host: 'random-data-api.com',
+      path: 'api/coffee/random_coffee',
+      queryParameters: {'size': sizeSelected.toString()},
+    );
 
     print('carregarCafes #1 - antes do await');
     var jsonString = await http.read(coffeesUri);
@@ -47,10 +49,11 @@ class DataService {
 
   Future<void> carregarCervejas() async {
     var beersUri = Uri(
-        scheme: 'https',
-        host: 'random-data-api.com',
-        path: 'api/beer/random_beer',
-        queryParameters: {'size': '5'});
+      scheme: 'https',
+      host: 'random-data-api.com',
+      path: 'api/beer/random_beer',
+      queryParameters: {'size': sizeSelected.toString()},
+    );
 
     print('carregarCervejas #1 - antes do await');
     var jsonString = await http.read(beersUri);
@@ -62,10 +65,11 @@ class DataService {
 
   Future<void> carregarNacoes() async {
     var nationsUri = Uri(
-        scheme: 'https',
-        host: 'random-data-api.com',
-        path: 'api/nation/random_nation',
-        queryParameters: {'size': '5'});
+      scheme: 'https',
+      host: 'random-data-api.com',
+      path: 'api/nation/random_nation',
+      queryParameters: {'size': sizeSelected.toString()},
+    );
 
     print('carregarNacoes #1 - antes do await');
     var jsonString = await http.read(nationsUri);
@@ -87,54 +91,82 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        theme: ThemeData(primarySwatch: Colors.deepPurple),
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          appBar: AppBar(
-            title: const Text("Dicas"),
-          ),
-          body: ValueListenableBuilder(
-              valueListenable: dataService.tableStateNotifier,
-              builder: (_, value, __) {
-                return DataTableWidget(
-                    jsonObjects: value,
-                    columnNames: dataService.columnNames,
-                    propertyNames: dataService.propertyNames);
-              }),
-          bottomNavigationBar:
-              NewNavBar(itemSelectedCallback: dataService.carregar),
-        ));
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Listas"),
+          actions: [
+            PopupMenuButton<int>(
+              icon: const Icon(Icons.filter_list),
+              itemBuilder: (context) => const [
+                PopupMenuItem(
+                  value: 5,
+                  child: Text("Carregar 5 itens"),
+                ),
+                PopupMenuItem(
+                  value: 10,
+                  child: Text("Carregar 10 itens"),
+                ),
+                PopupMenuItem(
+                  value: 15,
+                  child: Text("Carregar 15 itens"),
+                ),
+              ],
+              onSelected: (value) {
+                dataService.sizeSelected = value;
+                dataService.carregar;
+              },
+            ),
+          ],
+        ),
+        body: ValueListenableBuilder(
+          valueListenable: dataService.tableStateNotifier,
+          builder: (_, value, __) {
+            return DataTableWidget(
+              jsonObjects: value,
+              columnNames: dataService.columnNames,
+              propertyNames: dataService.propertyNames,
+            );
+          },
+        ),
+        bottomNavigationBar: NewNavBar(
+          itemSelectedCallback: dataService.carregar,
+        ),
+      ),
+    );
   }
 }
 
 class NewNavBar extends HookWidget {
   dynamic itemSelectedCallback;
-
   NewNavBar({this.itemSelectedCallback}) {
     itemSelectedCallback ??= (_) {};
   }
-
   @override
   Widget build(BuildContext context) {
     var state = useState(1);
-
     return BottomNavigationBar(
-        onTap: (index) {
-          state.value = index;
-
-          itemSelectedCallback(index);
-        },
-        currentIndex: state.value,
-        items: const [
-          BottomNavigationBarItem(
-            label: "Cafés",
-            icon: Icon(Icons.coffee_outlined),
-          ),
-          BottomNavigationBarItem(
-              label: "Cervejas", icon: Icon(Icons.local_drink_outlined)),
-          BottomNavigationBarItem(
-              label: "Nações", icon: Icon(Icons.flag_outlined))
-        ]);
+      onTap: (index) {
+        state.value = index;
+        itemSelectedCallback(index);
+      },
+      currentIndex: state.value,
+      items: const [
+        BottomNavigationBarItem(
+          label: "Cafés",
+          icon: Icon(Icons.coffee_outlined),
+        ),
+        BottomNavigationBarItem(
+          label: "Cervejas",
+          icon: Icon(Icons.local_drink_outlined),
+        ),
+        BottomNavigationBarItem(
+          label: "Nações",
+          icon: Icon(Icons.flag_outlined),
+        )
+      ],
+    );
   }
 }
 
