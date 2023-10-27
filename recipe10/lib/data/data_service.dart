7a7c2475c14716b59273bcbba9bad9f20921456f
quirinof <matheusquinoff@gmail.com>
2023-10-27 17:08:20 -0300

    starting recipe10 - base code

---
+++ recipe10/lib/data/data_service.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+enum TableStatus { idle, loading, ready, error }
+
+enum ItemType{
+  beer, coffee, nation, none;
+  String get asString => '$name';
+  List<String> get columns => 
+    this == coffee? ["Nome", "Origem", "Tipo"] :
+    this == beer? ["Nome", "Estilo", "IBU"]:
+    this == nation? ["Nome", "Capital", "Idioma","Esporte"]:
+    [] ;
+  List<String> get properties => 
+    this == coffee? ["blend_name","origin","variety"] :
+    this == beer? ["name","style","ibu"]:
+    this == nation? ["nationality","capital","language","national_sport"]:
+    [] ;
+}
+
+class DataService {
+  static const MAX_N_ITEMS = 15;
+  static const MIN_N_ITEMS = 3;
+  static const DEFAULT_N_ITEMS = 7;
+
+  int _numberOfItems = DEFAULT_N_ITEMS;
+  List _possibleNumbers = [MIN_N_ITEMS, DEFAULT_N_ITEMS, MAX_N_ITEMS];
+
+  int get numberOfItems {
+    return _numberOfItems;
+  }
+
+  set numberOfItems(n) {
+    _numberOfItems = n < 0 ? MIN_N_ITEMS : n > MAX_N_ITEMS ? MAX_N_ITEMS : n;
+  }
+
+  List get possibleNumbers {
+    return _possibleNumbers;
+  }
+  
+  set possibleNumbers(n) {
+    _possibleNumbers = possibleNumbers;
+  }
+
+  final ValueNotifier<Map<String,dynamic>> tableStateNotifier = ValueNotifier({
+    'status':TableStatus.idle,
+    'dataObjects':[],
+    'itemType': ItemType.none
+  });
+
+  void carregar(index){
+    final params = [ItemType.coffee, ItemType.beer, ItemType.nation];
+    carregarPorTipo(params[index]);
+  }
+
+  void emitirEstadoCarregando(ItemType type){
+    tableStateNotifier.value = {
+      'status': TableStatus.loading,
+      'dataObjects': [],
+      'itemType': type
+    };
+  }
+
+  Uri montarUri(ItemType type){
+    return Uri(
+      scheme: 'https',
+      host: 'random-data-api.com',
+      path: 'api/${type.asString}/random_${type.asString}',
+      queryParameters: {'size': '$_numberOfItems'});
+  }
+
+  Future<List<dynamic>> acessarApi(Uri uri) async{
+    var jsonString = await http.read(uri);
+    var json = jsonDecode(jsonString);
+    json = [...tableStateNotifier.value['dataObjects'], ...json];
+    return json;
+  }
+
+  void emitirEstadoPronto(ItemType type, var json){
+    tableStateNotifier.value = {
+      'itemType': type,
+      'status': TableStatus.ready,
+      'dataObjects': json,
+      'propertyNames': type.properties,
+      'columnNames': type.columns
+    };
+  }
+
+  bool temRequisicaoEmCurso() => tableStateNotifier.value['status'] == TableStatus.loading;
+  bool mudouTipoDeItemRequisitado(ItemType type) => tableStateNotifier.value['itemType'] != type;
+
+  void carregarPorTipo(ItemType type) async{
+    //ignorar solicitação se uma requisição já estiver em curso
+    if (temRequisicaoEmCurso()) return;
+    if (mudouTipoDeItemRequisitado(type)){
+      emitirEstadoCarregando(type);
+    }
+    var uri = montarUri(type);
+    var json = await acessarApi(uri);
+    emitirEstadoPronto(type, json);
+  }
+}
+
+final dataService = DataService();
---
+++ recipe10/lib/view/widgets.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import '../data/data_service.dart';
+
+class MyApp extends HookWidget {
+  @override
+  Widget build(BuildContext context) {
+    var popUpState = useState(dataService.numberOfItems);
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Dicas"),
+          actions: [
+            PopupMenuButton(
+              initialValue: popUpState.value,
+              itemBuilder: (_) => dataService.possibleNumbers.map(
+                (num) => PopupMenuItem(
+                  value: num,
+                  child: Text("Carregar $num itens por vez"),
+                )
+              ).toList(),
+              onSelected: (number) {
+                dataService.numberOfItems = number;
+                popUpState.value = dataService.numberOfItems;
+              },
+            )
+          ],
+        ),
+        body: ValueListenableBuilder(
+          valueListenable: dataService.tableStateNotifier,
+          builder: (_, value, __) {
+            switch (value['status']) {
+              case TableStatus.idle:
+                return Center(child: Text("Toque em algum botão"));
+
+              case TableStatus.loading:
+                return Center(child: CircularProgressIndicator());
+
+              case TableStatus.ready:
+                return InteractiveViewer(
+                  constrained: false,
+                  child: DataTableWidget(
+                    jsonObjects: value['dataObjects'],
+                    propertyNames: value['propertyNames'],
+                    columnNames: value['columnNames'],
+                  ),
+                );
+
+              case TableStatus.error:
+                return Text("Lascou");
+            }
+
+            return Text("...");
+          },
+        ),
+        bottomNavigationBar: NewNavBar(
+          itemSelectedCallback: dataService.carregar,
+        ),
+      ),
+    );
+  }
+}
+
+class NewNavBar extends HookWidget {
+  final _itemSelectedCallback;
+
+  NewNavBar({itemSelectedCallback})
+      : _itemSelectedCallback = itemSelectedCallback ?? (int) {}
+
+  @override
+  Widget build(BuildContext context) {
+    var state = useState(1);
+
+    return BottomNavigationBar(
+      onTap: (index) {
+        state.value = index;
+        _itemSelectedCallback(index);
+      },
+      currentIndex: state.value,
+      items: const [
+        BottomNavigationBarItem(
+          label: "Cafés",
+          icon: Icon(Icons.coffee_outlined),
+        ),
+        BottomNavigationBarItem(
+          label: "Cervejas",
+          icon: Icon(Icons.local_drink_outlined),
+        ),
+        BottomNavigationBarItem(
+          label: "Nações",
+          icon: Icon(Icons.flag_outlined),
+        )
+      ],
+    );
+  }
+}
+
+class DataTableWidget extends StatelessWidget {
+  final List jsonObjects;
+  final List<String> columnNames;
+  final List<String> propertyNames;
+
+  DataTableWidget({
+    this.jsonObjects = const [],
+    this.columnNames = const [],
+    this.propertyNames = const [],
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return DataTable(
+      columns: columnNames.map(
+        (name) => DataColumn(
+          label: Expanded(
+            child: Text(name, style: TextStyle(fontStyle: FontStyle.italic))
+          )
+        )
+      ).toList(),
+      rows: jsonObjects.map(
+        (obj) => DataRow(
+          cells: propertyNames.map(
+            (propName) => DataCell(Text(obj[propName]))
+          ).toList()
+        )
+      ).toList()
+    );
+  }
+}
