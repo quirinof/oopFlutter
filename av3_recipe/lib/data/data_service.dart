0d647425b2b4e9e92cf2a8f06a62d10689c4b6bd
quirinof <matheusquinoff@gmail.com>
2023-12-18 12:58:41 -0300

    recipe av3

---
+++ av3_recipe/lib/data/data_service.dart
@@ -0,0 +1,176 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import '../util/ordenador.dart';
+import '../util/filtrador.dart';
+
+enum TableStatus { idle, loading, ready, error }
+
+enum ItemType{
+  device, computer, app, none;
+  String get asString => '$name';
+  List<String> get columns => this == device? ["Nome", "Modelo", "Plataforma"] :
+                              this == computer? ["Plataforma", "Tipo", "OS"]:
+                              this == app? ["Nome", "Versão", "Autor"]:
+                              [] ;
+  List<String> get properties => this == device? ["manufacturer","model","platform"] :
+                                 this == computer? ["platform","type","os"]:
+                                 this == app? ["app_name","app_version","app_author"]:
+                                 [] ;
+}
+
+class DataService {
+  static const MAX_N_ITEMS = 15;
+  static const MIN_N_ITEMS = 3;
+  static const DEFAULT_N_ITEMS = 7;
+
+  int _numberOfItems = DEFAULT_N_ITEMS;
+  List _possibleNumbers = [MIN_N_ITEMS, DEFAULT_N_ITEMS, MAX_N_ITEMS];
+
+  int get numberOfItems {
+    return _numberOfItems;
+  }
+
+  set numberOfItems(n) {
+    _numberOfItems = n < 0 ? MIN_N_ITEMS : n > MAX_N_ITEMS ? MAX_N_ITEMS : n;
+  }
+
+  List get possibleNumbers {
+    return _possibleNumbers;
+  }
+  
+  set possibleNumbers(n) {
+    _possibleNumbers = possibleNumbers;
+  }
+
+  final ValueNotifier<Map<String,dynamic>> tableStateNotifier = ValueNotifier({
+    'status':TableStatus.idle,
+    'dataObjects':[],
+    'itemType': ItemType.none
+  });
+
+  void carregar(index){
+    final params = [ItemType.device, ItemType.computer, ItemType.app];
+    carregarPorTipo(params[index]);
+  }
+
+  void ordenarEstadoAtual(final String propriedade, final bool ascending){
+    List objetos =  tableStateNotifier.value['dataObjects'] ?? [];
+    if (objetos == []) return;
+    Ordenador ord = Ordenador();
+    Decididor d = ComparadorJSON(propriedade, ascending);
+    var objetosOrdenados = ord.ordenarFuderoso(objetos, d.precisaTrocarAtualPeloProximo);
+    
+    emitirEstadoOrdenado(objetosOrdenados, propriedade, ascending);
+  }
+
+  List filtrarObjetos() {
+    String filtro = tableStateNotifier.value['filterCriteria'];
+    List objetos = tableStateNotifier.value['dataObjects'] ?? [];
+    if (objetos == []) return [];
+    List propriedades = tableStateNotifier.value['propertyNames'];
+    Filtrador fil = Filtrador();
+    FilterDecider d = DecididorFiltroJSON(propriedades);
+    List objetosFiltrados = fil.filtrar(objetos, filtro, d.dentroDoFiltro);
+
+    return objetosFiltrados;
+  }
+
+  void emitirEstadoCarregando(ItemType type){
+    tableStateNotifier.value = {
+      'status': TableStatus.loading,
+      'dataObjects': [],
+      'itemType': type,
+      'filterCriteria': ''
+    };
+  }
+
+  Uri montarUri(ItemType type){
+    return Uri(
+      scheme: 'https',
+      host: 'random-data-api.com',
+      path: 'api/${type.asString}/random_${type.asString}',
+      queryParameters: {'size': '$_numberOfItems'});
+  }
+
+  Future<List<dynamic>> acessarApi(Uri uri) async{
+    var jsonString = await http.read(uri);
+    var json = jsonDecode(jsonString);
+    json = [...tableStateNotifier.value['dataObjects'], ...json];
+    return json;
+  }
+
+  void emitirEstadoOrdenado(List objetosOrdenados, String propriedade, bool ascending){
+    var estado = Map<String, dynamic>.from(tableStateNotifier.value);
+    estado['dataObjects'] = objetosOrdenados;
+    estado['sortCriteria'] = propriedade;
+    estado['ascending'] = ascending;
+    tableStateNotifier.value = estado;
+  }
+
+  void emitirEstadoFiltrado(String filtro) {
+    var estado = Map<String, dynamic>.from(tableStateNotifier.value);
+    estado['filterCriteria'] = filtro;
+    tableStateNotifier.value = estado;
+  }
+
+  void emitirEstadoPronto(ItemType type, var json){
+    tableStateNotifier.value = {
+      'itemType': type,
+      'status': TableStatus.ready,
+      'dataObjects': json,
+      'propertyNames': type.properties,
+      'columnNames': type.columns,
+      'filterCriteria': ''
+    };
+  }
+
+  bool temRequisicaoEmCurso() => tableStateNotifier.value['status'] == TableStatus.loading;
+  bool mudouTipoDeItemRequisitado(ItemType type) => tableStateNotifier.value['itemType'] != type;
+
+  void carregarPorTipo(ItemType type) async{
+    //ignorar solicitação se uma requisição já estiver em curso
+    if (temRequisicaoEmCurso()) return;
+    if (mudouTipoDeItemRequisitado(type)){
+      emitirEstadoCarregando(type);
+    }
+    var uri = montarUri(type);
+    var json = await acessarApi(uri);
+    emitirEstadoPronto(type, json);
+  }
+}
+
+final dataService = DataService();
+
+class ComparadorJSON extends Decididor{
+  final String propriedade;
+  final bool crescente;
+
+  ComparadorJSON(this.propriedade, [this.crescente = true]);
+
+  @override
+  bool precisaTrocarAtualPeloProximo(atual, proximo) {
+    try{
+      final ordemCorreta = crescente ? [atual, proximo] : [proximo, atual];
+      return ordemCorreta[0][propriedade].compareTo(ordemCorreta[1][propriedade]) > 0;
+    }catch (error){
+      return false;
+    }    
+  }
+}
+
+class DecididorFiltroJSON extends FilterDecider {
+  final List propriedades;
+
+  DecididorFiltroJSON(this.propriedades);
+
+  @override
+  bool dentroDoFiltro(objeto, filtro) {
+    bool achouAoMenosUm = false;
+    for (int i=0; i<propriedades.length-1; i++) {
+      achouAoMenosUm = objeto[propriedades[i]].contains(filtro) ? true : false;
+      if (achouAoMenosUm) break;
+    }
+    return achouAoMenosUm;
+  }
+}
---
+++ av3_recipe/lib/view/widgets.dart
@@ -0,0 +1,158 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import '../data/data_service.dart';
+class MyApp extends HookWidget {
+  @override
+  Widget build(BuildContext context) {
+    var popUpState = useState(dataService.numberOfItems);
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("POO 1"),
+          actions: [
+            SearchBar(
+              leading: const Icon(
+                Icons.search,
+                color: Colors.grey,
+              ),
+              constraints: const BoxConstraints(
+                minWidth: 1.0,
+                maxWidth: 300.0,
+              ),
+              onChanged: (filter) {
+                if (filter.length >= 3) {
+                  dataService.emitirEstadoFiltrado(filter);
+                }
+                else {
+                  dataService.emitirEstadoFiltrado('');
+                }
+              },
+            ),
+            PopupMenuButton(
+              initialValue: popUpState.value,
+              itemBuilder: (_) => dataService.possibleNumbers.map(
+                (num) => PopupMenuItem(
+                  value: num,
+                  child: Text("Carregar $num itens por vez"),
+                )
+              ).toList(),
+              onSelected: (number) {
+                dataService.numberOfItems = number;
+                popUpState.value = dataService.numberOfItems;
+              },
+            )
+          ],
+        ),
+        body: ValueListenableBuilder(
+          valueListenable: dataService.tableStateNotifier,
+          builder: (_, value, __) {
+            switch (value['status']) {
+              case TableStatus.idle:
+                return Center(child: Text("Toque em algum botão"));
+
+              case TableStatus.loading:
+                return Center(child: CircularProgressIndicator());
+
+              case TableStatus.ready:
+                return InteractiveViewer(
+                  constrained: false,
+                  child: DataTableWidget(
+                    jsonObjects: dataService.filtrarObjetos(),
+                    propertyNames: value['propertyNames'],
+                    columnNames: value['columnNames'],
+                    sortCallback: dataService.ordenarEstadoAtual
+                  ),
+                );
+
+              case TableStatus.error:
+                return Text("Lascou");
+            }
+
+            return Text("...");
+          },
+        ),
+        bottomNavigationBar: NewNavBar(
+          itemSelectedCallback: dataService.carregar,
+        ),
+      ),
+    );
+  }
+}
+
+class NewNavBar extends HookWidget {
+  final _itemSelectedCallback;
+
+  NewNavBar({itemSelectedCallback})
+      : _itemSelectedCallback = itemSelectedCallback ?? (int) {}
+
+  @override
+  Widget build(BuildContext context) {
+    var state = useState(1);
+
+    return BottomNavigationBar(
+      onTap: (index) {
+        state.value = index;
+        _itemSelectedCallback(index);
+      },
+      currentIndex: state.value,
+      items: const [
+        BottomNavigationBarItem(
+          label: "Dispositivos",
+          icon: Icon(Icons.devices_other_outlined),
+        ),
+        BottomNavigationBarItem(
+          label: "Computadores",
+          icon: Icon(Icons.computer_outlined),
+        ),
+        BottomNavigationBarItem(
+          label: "Aplicativos",
+          icon: Icon(Icons.apps_outage_outlined),
+        )
+      ],
+    );
+  }
+}
+
+class DataTableWidget extends HookWidget {
+  final List jsonObjects;
+  final List<String> columnNames;
+  final List<String> propertyNames;
+  final _sortCallback;
+
+  DataTableWidget({
+    this.jsonObjects = const [], 
+    this.columnNames = const [], 
+    this.propertyNames= const [], sortCallback
+  }) : _sortCallback = sortCallback ?? (String, bool){}
+
+  @override
+  Widget build(BuildContext context) {
+    var isAscending = useState(false);
+    var sortedColumn = useState(0);
+    return DataTable(
+      sortAscending: isAscending.value,
+      sortColumnIndex: sortedColumn.value,
+      columns: columnNames.map(
+        (name) => DataColumn(
+          onSort: (columnIndex, ascending) {
+            _sortCallback(propertyNames[columnIndex], ascending);
+            sortedColumn.value = columnIndex;
+            isAscending.value = ascending;
+          },
+          label: Expanded(
+            child: Text(name, style: TextStyle(fontStyle: FontStyle.italic))
+          )
+        )
+      ).toList(),
+      rows: jsonObjects.map(
+        (obj) => DataRow(
+          cells: propertyNames.map(
+            (propName) => DataCell(Text(obj[propName]))
+          ).toList()
+        )
+      ).toList()
+    );
+  }
+}
