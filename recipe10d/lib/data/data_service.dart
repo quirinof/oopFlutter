b30aa4d23ee15bccf6a508e9cb806aba7aa509fd
quirinof <matheusquinoff@gmail.com>
2023-12-18 12:58:25 -0300

    att recipe10d

--- recipe10d/lib/data/data_service.dart
+++ recipe10d/lib/data/data_service.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import '../util/ordenador.dart';
+import '../data/models.dart';
 
 enum TableStatus { idle, loading, ready, error }
 
